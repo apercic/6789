d72d94ad588ac5ae7dc685e9b39e49e1d3f730e8
eee <voocrzamcjqaudsrla@pptrvv.com>
2021-11-02 19:51:19 +0100

    base commit

---
+++ src/main/java/com/game/game/GameEntity.java
@@ -0,0 +1,13 @@
+package com.game.game;
+
+class GameEntity {
+
+    private Long id;
+    private byte[] state;
+    private String turn;
+    private Boolean win;
+
+    private String player1;
+    private String player2;
+
+}
